e4e240abfc9adbf9409915c47f9b4648e9d9c548
Zeztron <pateltharsh@gmail.com>
2023-03-20 17:58:16 -0500

    sum array

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -16,8 +16,20 @@ fn main() {
 
     let d = add_two(c, parsed_guess);
     println!("d = {d}");
+
+    let arr = [1, 2, 3, 4, 5];
+    let sum = sum_array(arr);
+    println!("sum = {sum}");
 }
 
 fn add_two(a: i32, b: i32) -> i32 {
     return a + b;
+}
+
+fn sum_array(arr: [i32; 5]) -> i32 {
+    let mut sum = 0;
+    for i in arr.iter() {
+        sum += i;
+    }
+    return sum;
 }
