fbdb84d1569e1ca17a1ce5e1582ed4114990efab
Zeztron <pateltharsh@gmail.com>
2023-03-27 18:27:17 -0500

    match

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,37 +1,26 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
-    let c: i32 = 5;
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let parsed_guess = guess.trim().parse().expect("error");
+    println!("You guessed: {}", guess);
 
-    let d = add_two(c, parsed_guess);
-
-    println!("d = {d}");
-
-    let arr = [1, 2, 3, 4, 5];
-
-    let sum = sum_array(arr);
-    println!("sum = {sum}");
-}
-
-fn add_two(a: i32, b: i32) -> i32 {
-    return a + b;
-}
-
-fn sum_array(arr: [i32; 5]) -> i32 {
-    let mut sum = 0;
-    for i in arr.iter() {
-        sum += i;
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
     }
-    return sum;
 }
